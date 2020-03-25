011b6eff92003d1f1fca0c76870ea334852a749f
Thembiso Ragimana <41780221+Thembiso@users.noreply.github.com>
2020-03-25 19:33:57 +0200

    core backend code deployed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(logger('combined'));
 app.use('/', indexRouter);
 app.use('/usersold', usersRouter);
 //importing route
