abb176f4bb99b2eb7381d83b35d941c75114f591
Thembiso Ragimana <41780221+Thembiso@users.noreply.github.com>
2020-03-25 18:25:46 +0200

    user list all done

--- _coreApplication/routes/index.js
+++ _coreApplication/routes/index.js
@@ -4,12 +4,5 @@ module.exports = function(app) {
   //add users mgt route file path
   var usersRoute = require("./UserManagement");
   app.use("/api/users" , usersRoute);
-
-  //add order mgt file path
-  var productRoute = require("./Product_Management");
-  app.use("/api/product" , productRoute);
-
-  //add order mgt file path
-  var orderRoute = require("./OrderManagement");
-  app.use("/api/orders" , orderRoute);
+  
 };
